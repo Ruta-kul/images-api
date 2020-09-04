2e32c202b6526d3e315388e41ab1e3b7b34fad6e
Ruta Kulkarni <rutakulkarni08@gmail.com>
2020-09-04 18:23:22 -0400

    message upload and slack integrated

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const path = require("path");
+var request = require('request');
+var fs = require('fs');
+
+
+const {WebClient} = require('@slack/web-api');
+
+const token = process.env.SLACK_BOT_TOKEN;
+const web = new WebClient(token);
+
+const conversationId = '#images-api';
+
+
 
 
 // storage engine
@@ -16,13 +28,30 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage
 })
+
 app.use('/profile', express.static('upload/image'));
 app.post("/upload", upload.single('profile'), (req, res)=>{
-    res.json({
-        success: 1,
-        profile_url: `http://localhost:4000/profile/${req.file.filename}`
-    })
+        try {
+            (async () => {
+
+                // const res = await web.chat.postMessage({channel: conversationId, text: 'Hello there'});
+                // console.log('Message sent:', res.ts);
+
+                const result = await web.files.upload({
+                    // The token you used to initialize your app is stored in the `context` object
+                    token: token,
+                    channels: conversationId,
+                    initial_comment: "Here\'s my file :smile:",
+                    // Include your filename in a ReadStream here
+                    file: fs.createReadStream('./upload/image/'+req.file.filename)
+                  });
+            })();
+        }
+          catch (error) {
+            console.error(error);
+          }
 })
+
 
 app.listen(4000,()=>{
     console.log("server up and running");
